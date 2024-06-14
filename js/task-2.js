893cff5ad89bfcddaa7ff1cb02d95a55e1262f6b
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-06-14 15:44:51 +0200

    2 done

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,27 @@
 'use strict';
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+const markup = images
+  .map(image => `<li> <img url="${image.url}" alt="${image.alt}"> </li>`)
+  .join('');
+
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('afterbegin', markup);
+console.log(markup);
+
+gallery.style.display = 'flex';
+gallery.style.flexDirection = 'column';
+gallery.style.listStyle = 'none';
+gallery.style.gap = '25px';
